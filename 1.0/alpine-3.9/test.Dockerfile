64b2e3b20a6a328ce2edeef90621e7ac40c8d4f8
Andrzej Klajnert <github@aklajnert.pl>
2019-03-30 15:07:37 +0100

    Code reorganization.

--- 1.0/alpine-3.9/test.Dockerfile
+++ 1.0/alpine-3.9/test.Dockerfile
@@ -1,6 +1,6 @@
 FROM snakepit:1.0-alpine-3.9
 
-ADD . /tests
+ADD ./tests /tests
 WORKDIR /tests
 
 RUN pip install tox
--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
-cd 1.0-alpine-3.9
+cd 1.0/alpine-3.9
 docker build -t snakepit:1.0-alpine-3.9 .
 
-cd tests
-docker build .
+cd ..
+docker build . -f alpine-3.9/test.Dockerfile
