031a499f2f935b4d0dfdcb3640bc193811922499
Andrzej Klajnert <github@aklajnert.pl>
2019-03-30 16:22:05 +0100

    Prepared files for stretch-slim based image.

--- build.sh
+++ build.sh
@@ -3,6 +3,15 @@ set -ex
 
 cd 1.0/alpine-3.9
 docker build -t snakepit:1.0-alpine-3.9 .
+docker tag snakepit:1.0-alpine-3.9 aklajnert/1.0-alpine-3.9
+
+
+cd ..
+docker build . -f alpine-3.9/test.Dockerfile
+
+cd 1.0/stretch-slim
+docker build -t snakepit:1.0-stretch-slim .
+docker tag snakepit:1.0-stretch-slim aklajnert/snakepit:1.0-stretch-slim
 
 cd ..
 docker build . -f alpine-3.9/test.Dockerfile
